280b4d7a91eafd304c9e798c1a79869e096c94b2
blackALT <wanessapssouza@gmail.com>
2020-11-07 23:24:31 -0300

    controller

--- src/controllers/clientesController.js
+++ src/controllers/clientesController.js
@@ -1,21 +1,56 @@
+const clientes = require('../models/clientes')
+
 const getAll = (req, res) => {
-    console.log("getAll");
-      res.status(200).send("ok");
+  console.log(req.url);
+  clientes.find(function (err, clientes) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    } else {
+      res.status(200).send(clientes)
+    }
+  })
 };
 
 const getCompradores = (req, res) => {
-    console.log("getCompradores");
-      res.status(200).send("ok");
+  console.log(req.url);
+  clientes.find({ comprou: true }, function (err, clientes) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    } else {
+      const clientesFilter = clientes.map(cliente => {
+        return {
+          nome: cliente.nome,
+          email: cliente.email
+        }
+      })
+      res.status(200).send(clientesFilter);
+    }
+  })
 };
 
 const getByCpf = (req, res) => {
-    console.log("getByCpf");
-      res.status(200).send("ok");
+  const cpf = req.params.cpf
+  clientes.find({ cpf }, function (err, clientes) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    } else {
+      res.status(200).send(clientes);
+    }
+  })
 };
 
 const postCliente = (req, res) => {
-    console.log("postCliente");
-      res.status(200).send("ok");
+  let cliente = new clientes(req.body)
+  cliente.save(function (err) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    } else {
+      res.status(201).send({
+        message: "Cliente incluido com sucesso",
+        status: "true"
+      })
+    }
+  })
 };
 
 module.exports = {
